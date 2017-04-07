550065334e8fc4fec76cf169eaaf4dea83a05656
Benjamin T. Seaver <benjamintseaver@gmail.com>
2017-04-07 16:23:06 -0700

    Add content to member profiles page

--- src/app/profiles/profiles.component.ts
+++ src/app/profiles/profiles.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
+import { Location } from '@angular/common';
+import { Member } from '../member.model';
+import { MemberService } from '../member.service';
+import { FirebaseObjectObservable } from 'angularfire2';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-profiles',
   templateUrl: './profiles.component.html',
-  styleUrls: ['./profiles.component.css']
+  styleUrls: ['./profiles.component.css'],
+  providers: [MemberService]
 })
-export class ProfilesComponent implements OnInit {
 
-  constructor() { }
+export class ProfilesComponent implements OnInit {
+  memberId: string = null;
+  thisMember;
+
+  constructor(
+    private router: Router,
+    private route:  ActivatedRoute,
+    private location: Location,
+    private memberService: MemberService
+  ) { }
+
 
   ngOnInit() {
+    this.route.params.forEach((urlParameter) => {
+      this.memberId = urlParameter['id'];
+    });
+
+    this.memberService.getMemberById(this.memberId).subscribe(dataLastEmittedFromObserver => {
+      this.thisMember = new Member(
+        dataLastEmittedFromObserver.memberName,
+        dataLastEmittedFromObserver.memberSince,
+        dataLastEmittedFromObserver.memberProfile,
+        dataLastEmittedFromObserver.clubOfficer
+      );
+    });
   }
 
 }
