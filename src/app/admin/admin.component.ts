fa9e7b4130e54a25a5cf3c0391d9e250cc504017
Benjamin T. Seaver <benjamintseaver@gmail.com>
2017-04-07 16:53:31 -0700

    Add table content to admin page

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Member } from './../member.model';
+import { Router } from '@angular/router';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { MemberService } from './../member.service';
+
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.css']
+  styleUrls: ['./admin.component.css'],
+  providers: [MemberService]
 })
 export class AdminComponent implements OnInit {
+  members: FirebaseListObservable<any[]>;
+  filterByOfficer = 'all';
 
-  constructor() { }
+  constructor(private router: Router, private memberService: MemberService) { }
 
   ngOnInit() {
+    this.members = this.memberService.getMembers();
   }
 
+  // viewProfile(thisMember) {
+  //   this.router.navigate(['profiles', thisMember.$key]);
+  // }
+
+  filterChange(selection: string) {
+    this.filterByOfficer = selection;
+  }
 }
